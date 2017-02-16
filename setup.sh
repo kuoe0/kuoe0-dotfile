91bcd8540475c4653f9e3ff3a0297421bf5434c3
KuoE0 <kuoe0.tw@gmail.com>
2017-02-16 13:26:12 +0800

    setup tmux

--- setup.sh
+++ setup.sh
@@ -25,18 +25,23 @@ else
 	SCRIPTPATH=$(readlink -f "$0" | xargs -0 dirname)
 fi
 
+ln -s "$SCRIPTPATH/dot.gitconfig" "$HOME/.gitconfig"
+
 echo "Installing gdb-dashboard..."
 git clone --depth 1 https://github.com/cyrus-and/gdb-dashboard "$SCRIPTPATH/src/gdb-dashboard"
 ln -s "$HOME/gdb-dashboard/.gdbinit" "$HOME/.gdbinit"
-
-echo "Setting up Zsh environment..."
-git clone git@github.com:KuoE0/KuoE0-zsh.git "$WORK_DIR/KuoE0-zsh"
-cd "$WORK_DIR/KuoE0-zsh"
-./setup.sh
 
 echo "Settting up Vim environment..."
 git clone git@github.com:KuoE0/KuoE0-vim.git "$WORK_DIR/KuoE0-vim"
 cd "$WORK_DIR/KuoE0-vim"
 ./setup.sh
 
-ln -s "$SCRIPTPATH/dot.gitconfig" "$HOME/.gitconfig"
+echo "Settting up tmux environment..."
+git clone git@github.com:KuoE0/KuoE0-tmux.git "$WORK_DIR/KuoE0-tmux"
+cd "$WORK_DIR/KuoE0-tmux"
+./setup.sh
+
+echo "Setting up Zsh environment..."
+git clone git@github.com:KuoE0/KuoE0-zsh.git "$WORK_DIR/KuoE0-zsh"
+cd "$WORK_DIR/KuoE0-zsh"
+./setup.sh
