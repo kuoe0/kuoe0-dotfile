1839860ba1b9488acafecc261e50a2402530f433
KuoE0 <kuoe0.tw@gmail.com>
2017-08-25 18:32:13 +0800

    check folders exist or not

--- setup.sh
+++ setup.sh
@@ -27,21 +27,36 @@ fi
 
 ln -s "$SCRIPTPATH/dot.gitconfig" "$HOME/.gitconfig"
 
-echo "Installing gdb-dashboard..."
-git clone --depth 1 https://github.com/cyrus-and/gdb-dashboard "$WORK_DIR/gdb-dashboard"
-ln -s "$WORK_DIR/gdb-dashboard/.gdbinit" "$HOME/.gdbinit"
+if [[ -d "$WORK_DIR/gdb-dashboard" ]]; then
+	echo "gdb-dashboard already exists."
+else
+	echo "Installing gdb-dashboard..."
+	git clone --depth 1 https://github.com/cyrus-and/gdb-dashboard "$WORK_DIR/gdb-dashboard"
+	ln -s "$WORK_DIR/gdb-dashboard/.gdbinit" "$HOME/.gdbinit"
+fi
 
-echo "Settting up Vim environment..."
-git clone git@github.com:KuoE0/KuoE0-vim.git "$WORK_DIR/KuoE0-vim"
-cd "$WORK_DIR/KuoE0-vim"
-make all
+if [[ -d "$WORK_DIR/kuoe0-vim" ]]; then
+	echo "kuoe0-vim already exists." else
+	echo "Settting up Vim environment..."
+	git clone git@github.com:kuoe0/kuoe0-vim.git "$WORK_DIR/kuoe0-vim"
+	cd "$WORK_DIR/kuoe0-vim"
+	make all
+fi
 
-echo "Settting up tmux environment..."
-git clone git@github.com:KuoE0/KuoE0-tmux.git "$WORK_DIR/KuoE0-tmux"
-cd "$WORK_DIR/KuoE0-tmux"
-./setup.sh
+if [[ -d "$WORK_DIR/kuoe0-tmux" ]]; then
+	echo "kuoe0-tmux already exists."
+else
+	echo "Settting up tmux environment..."
+	git clone git@github.com:kuoe0/kuoe0-tmux.git "$WORK_DIR/kuoe0-tmux"
+	cd "$WORK_DIR/kuoe0-tmux"
+	./setup.sh
+fi
 
-echo "Setting up Zsh environment..."
-git clone git@github.com:KuoE0/KuoE0-zsh.git "$WORK_DIR/KuoE0-zsh"
-cd "$WORK_DIR/KuoE0-zsh"
-./setup.sh
+if [[ -d "$WORK_DIR/kuoe0-zsh" ]]; then
+	echo "kuoe0-zsh already exists."
+else
+	echo "Setting up Zsh environment..."
+	git clone git@github.com:kuoe0/kuoe0-zsh.git "$WORK_DIR/kuoe0-zsh"
+	cd "$WORK_DIR/kuoe0-zsh"
+	./setup.sh
+fi
